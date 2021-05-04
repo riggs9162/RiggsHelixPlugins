76c5a9329f1a31e43c9262c67ba41f11ccf6f2e4
Riggs <49407096+RiggsMackay@users.noreply.github.com>
2021-05-04 15:33:32 +0200

    Add files via upload

---
+++ allowflashlight.lua
@@ -0,0 +1,7 @@
+PLUGIN.name = "Allow Flashlights"
+PLUGIN.author = "Riggs"
+PLUGIN.description = "Allows everyone to use flashlights."
+
+function PLUGIN:PlayerSwitchFlashlight(client, bEnabled)
+	return true
+end
