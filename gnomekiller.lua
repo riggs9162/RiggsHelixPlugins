47fbe235a0818bef039894e70531d94c40e50640
Riggs <49407096+RiggsMackay@users.noreply.github.com>
2022-01-16 13:36:54 +0100

    Update gnomekiller.lua

--- gnomekiller.lua
+++ gnomekiller.lua
@@ -1,14 +1,18 @@
 local PLUGIN = PLUGIN
 
 PLUGIN.name = "Gnome Killer"
-PLUGIN.author = "ZIKE"
-PLUGIN.description = "Allows you to kill gnome props, and make a high pitched scream. :)"
+PLUGIN.author = "Riggs Mackay"
+PLUGIN.description = "Allows you to kill gnome props, and make a high pitched scream."
 
-function PLUGIN:EntityTakeDamage(ent, dmg)
-    if isentity(ent) then
-    	if ent:GetModel() == "models/props_junk/gnome.mdl" then
-    		ent:EmitSound("vo/npc/male01/pain0"..math.random(1,9)..".wav", 75, 150)
-    		ent:Remove()
-    	end
-	end
+local gnomeClasses = {
+    ["prop_physics"] = true,
+    ["ix_item"] = true,
+}
+function PLUGIN:EntityTakeDamage(ent)
+    if ( gnomeClasses[ent:GetClass()] ) then
+        if ( ent:GetModel():find("gnome") ) then
+            ent:EmitSound("vo/npc/male01/pain0"..math.random(1,9)..".wav", 75, 150)
+            ent:Remove()
+        end
+    end
 end
