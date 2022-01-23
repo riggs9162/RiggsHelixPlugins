c98fdecf9f327ea157de62826a6529e0371b8684
Riggs <49407096+RiggsMackay@users.noreply.github.com>
2022-01-23 11:45:09 +0100

    Update

--- hungersystem/items/base/sh_foods.lua
+++ hungersystem/items/base/sh_foods.lua
@@ -1,4 +1,3 @@
-
 ITEM.name = "Consumable Base"
 ITEM.model = Model("models/props_junk/garbage_takeoutcarton001a.mdl")
 ITEM.description = "A base for consumables."
@@ -10,54 +9,6 @@ ITEM.useSound = "npc/barnacle/barnacle_crunch2.wav"
 ITEM.useName = "Consume"
 
 ITEM.RestoreHunger = 0
-ITEM.RestoreHealth = 0
-ITEM.damage = 0
-ITEM.spoilTime = 14
-
-function ITEM:GetName()
-	if (self:GetSpoiled()) then
-		local spoilText = self.spoilText or "Spoiled"
-		return spoilText.." "..self.name
-	end
-
-	return self.name
-end
-
-function ITEM:GetDescription()
-	local description = {self.description}
-	if (!self:GetSpoiled() and self:GetData("spoilTime")) then
-		local spoilTime = math.floor((self:GetData("spoilTime") - os.time()) / 60)
-		local text = " minutes."
-		if (spoilTime > 60) then
-			text = " hours."
-			spoilTime = math.floor(spoilTime / 60)
-		end
-
-		if (spoilTime > 24) then
-			text = " days."
-			spoilTime = math.floor(spoilTime / 24)
-		end
-
-		description[#description + 1] = "\nSpoils in "..spoilTime..text
-	end
-
-	return table.concat(description, "")
-end
-
-function ITEM:GetSpoiled()
-	local spoilTime = self:GetData("spoilTime")
-	if (!spoilTime) then
-		return false
-	end
-
-	return os.time() > spoilTime
-end
-
-function ITEM:OnInstanced()
-	if (self.spoil) then
-		self:SetData("spoilTime", os.time() + 24 * 60 * 60 * self.spoilTime)
-	end
-end
 
 ITEM.functions.Consume = {
 	icon = "icon16/user.png",
@@ -69,12 +20,12 @@ ITEM.functions.Consume = {
 		local actiontext = "Invalid Action"
 
 		if ( ply.isEatingConsumeable == true ) then
-			ply:Notify("You can't stuff too much food in your mouth, bruh.")
+			ply:Notify("You can't stuff too much food in your mouth, bruh.") -- bruh
 			return false
 		end
 
 		if (item.useSound) then
-			if string.find(item.useSound, "drink") then
+			if ( string.find(item.useSound, "drink") ) then -- if you have custom sounds for drinking / eating, change this.
 				actiontext = "Drinking.."
 			else
 				actiontext = "Eating.."
@@ -82,40 +33,22 @@ ITEM.functions.Consume = {
 		end
 
 		local function EatFunction(ply, character, bSpoiled)
-			if not (ply:IsValid() and ply:Alive() and character) then return end
-
-			if (item.damage > 0) then
-				ply:TakeDamage(item.damage, ply, ply)
-			end
+			if not ( ply:IsValid() and ply:Alive() and character ) then return end
 	
-			if (item.junk) then
-				if (!character:GetInventory():Add(item.junk)) then
-					ix.item.Spawn(item.junk, ply)
-				end
-			end
-	
-			if (item.useSound) then
-				if (istable(item.useSound)) then
+			if ( item.useSound ) then
+				if ( istable(item.useSound) ) then
 					ply:EmitSound(table.Random(item.useSound))
 				else
 					ply:EmitSound(item.useSound)
 				end
 			end
 	
-			if (!bSpoiled) then
-				if (item.RestoreHunger > 0) then
-					character:SetHunger(math.Clamp(character:GetHunger() + item.RestoreHunger, 0, 100))
-				end
-	
-				if (item.RestoreHealth > 0) then
-					ply:SetHealth(math.Clamp(ply:Health() + item.RestoreHealth, 0, ply:GetMaxHealth()))
-				end
-			else
-				ply:TakeDamage(math.random(1,5))
+			if ( item.RestoreHunger > 0 ) then
+				character:SetHunger(math.Clamp(character:GetHunger() + item.RestoreHunger, 0, 100))
 			end
 		end
 
-		if (item.useTime) then
+		if ( item.useTime ) then
 			ply.isEatingConsumeable = true
 			ply:SetAction(actiontext, item.useTime, function()
 				EatFunction(ply, character, bSpoiled)
