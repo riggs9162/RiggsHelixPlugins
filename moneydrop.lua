8bfb46ebff22d9846e203094064bfa9977cffb54
Riggs <xconnorx@gmx.de>
2024-06-18 12:14:21 +0200

    fix

--- moneydrop.lua
+++ moneydrop.lua
@@ -19,7 +19,7 @@ function PLUGIN:InitializedConfig()
         category = "Money Drop"
     })
 
-    ix.config.Add("moneyDropAmount", 100, "The amount of money to drop on death. If set to 0, it will drop all money.", function(oldValue, newValue)
+    ix.config.Add("moneyDropAmount", 100, "The amount of money to drop on death. If set to 0, it will drop all money.", {
         data = {min = 0, max = 10000, decimals = 0}
         category = "Money Drop"
     })
